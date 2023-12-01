91ec05a5fbe23557c894eddc0d31547282b93c99
pievos101 <bastianxpfeifer@gmail.com>
2023-12-01 15:10:37 +0100

    cleaning

--- R/estimateTheta.R
+++ R/estimateTheta.R
@@ -2,21 +2,21 @@
 #'
 #' This is the main function for the estimation of the underlying latent signals. The parameters required are: (1) A rank matrix (objects as rows, assessors as columns), (2) the number of bootstrap samples (500 are recommended), and (3) a constant for the support variables \(b>0\), default is 0.1, (4)  the type of model with four different options: fullLinear, fullQuadratic, restrictedLinear, and restrictedQuadratic, (5) the type of bootstrap sampling scheme, poisson.bootstrap and classic.bootstrap, and (6) the number of cores for parallel computation. Each bootstrap sample is executed on a dedicated CPU core.
 #' 
-#' @param R.input A matrix where the number of rows is equal to the number of the objects and
-#' the number of columns equals the number of assessors (rankers).
+#' @param R.input A matrix where the rows represent the objects and
+#' the columns the assessors (rankers).
 #' @param b The penalization term. The suggested value is 0.1.
 #' @param num.boot The number of boostrap samples created from the input ranked matrix. A positive number is expected.
-#' @param solver A string that indicate which solver to use. Two options are available, 'gurobi' and 'nloptr'.
+#' @param solver A string that indicates which solver to use. Two options are available, 'gurobi' and 'nloptr'.
 #' We recommend to use gurobi for faster computation. Note, a licence is required. Check the corresponding documentation on how to install gurobi.
 #' @param type A string that indicates which model to use: four approaches are available: 'restrictedQuadratic', 'fullQuadratic', 'restrictedLinear' and 'fullLinear'.
-#' @param bootstrap.type  string that indicate which bootstrap method to use: 'classic.bootstrap' or 'poisson.bootstrap'.
+#' @param bootstrap.type  A string that indicates which bootstrap method to use: 'classic.bootstrap' or 'poisson.bootstrap'.
 #' @param nCore The number of cores used for computation. Each core is used to calculate the signals from a bootstrap sample. Default number is detectCores() - 1.
 #' @return A list with the estimation information obtained:
 #'  \itemize{
 #'   \item estimation - A data frame with the signal estimation and the standard error computed by the bootstrap for each object
 #'   \item estimatedMatrixNoise - The estimated matrix noise 
 #'   \item time - The execution time of the procedure
-#'   \item allBootstraps - The single bootstrap estimations of all objects for each bootstrap sample
+#'   \item allBootstraps - The signal estimates from all bootstrap iterations
 #' }
 #' @keywords estimateTheta
 #' @export
--- R/generate.rank.matrix.R
+++ R/generate.rank.matrix.R
@@ -3,11 +3,11 @@
 #' The generate.rank.matrix() function requires the user to specify the number of objects (items), called p, and the number of assessors (rankers), called n. The function simulates full ranked lists (i.e. no missing assignments) without ties.
 #' @param p The number of objects.
 #' @param n The number of assessors.
-#' @param percentageMissing The percentage of the missing values. Note, the current version of TopKSignal does not support missing data.
+#' @param percentageMissing The percentage of the missing values. Note, missing data should be resolved by the rank() function before calling estimateTheta().
 #' @return A list with simulated data
 #' \itemize{
 #'   \item R.input - The rank matrix 
-#'   \item thea.true - The true underlying signals from the assessments
+#'   \item thea.true - The true underlying signals from the assessments 
 #'   \item sigmas - The standard error of the noise added for each assessor
 #'   \item matrixNoise - The noise added to the true signals in order to get the final rank matrix
 #' }
