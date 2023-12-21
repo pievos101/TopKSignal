87e72c1bbe621e9a1728dfb7671ce8642a2a8e25
bastian <bastianxpfeifer@gmail.com>
2023-12-21 20:28:17 +0100

    set-up

---
+++ R/estimatedSignal.R
@@ -0,0 +1,12 @@
+#' Object returned by the estimateTheta() function.
+#'
+#'
+#' @format A list of various values returned by the estimateTheta() function.
+#' \describe{
+#'	 \item{estimation}{A data frame with the signal estimation and the standard error computed by the bootstrap for each object}
+#'   \item{estimatedMatrixNoise}{The estimated matrix noise} 
+#'   \item{allBootstraps}{The signal estimates from all bootstrap iterations}
+#' }
+#' @source na
+#' @name estimatedSignal
+NULL
